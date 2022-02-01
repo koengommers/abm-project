9aaaada56b695e6432ca41e966e6d6bef0888192
Koen Gommers <koen@koengommers.nl>
2022-02-01 20:54:04 +0100

    Improvements for better results

--- batchrunner.py
+++ batchrunner.py
@@ -1,6 +1,7 @@
 from SALib.sample import saltelli
 from multiprocess import Pool, cpu_count
 from mesa.batchrunner import BatchRunnerMP
+import pandas as pd
 
 class SobolBatchRunner(BatchRunnerMP):
     def __init__(self, model_cls, problem, distinct_samples, **kwargs):
@@ -11,3 +12,66 @@ class SobolBatchRunner(BatchRunnerMP):
         param_values = saltelli.sample(problem, distinct_samples)
         self.parameters_list = [{name: val for name, val in zip(problem['names'], vals)} for vals in param_values]
 
+    def _prepare_report_table(self, vars_dict, extra_cols=None):
+        """
+        Creates a dataframe from collected records and sorts it using 'Run'
+        column as a key.
+        """
+        extra_cols = ["Run"] + (extra_cols or [])
+        index_cols = []
+        if self.parameters_list:
+            index_cols = list(self.parameters_list[0].keys()) + list(self.fixed_parameters.keys())
+        index_cols += extra_cols
+
+        records = []
+        for param_key, values in vars_dict.items():
+            record = dict(zip(index_cols, param_key))
+            # print(record)
+            record.update(values)
+            records.append(record)
+
+        df = pd.DataFrame(records)
+        # print(records)
+        # print(df['Run'])
+        rest_cols = set(df.columns) - set(index_cols)
+        ordered = df[index_cols + list(sorted(rest_cols))]
+        ordered.sort_values(by="Run", inplace=True)
+        if self._include_fixed:
+            for param in self.fixed_parameters.keys():
+                val = self.fixed_parameters[param]
+
+                # avoid error when val is an iterable
+                vallist = [val for i in range(ordered.shape[0])]
+                ordered[param] = vallist
+        return ordered
+
+    def _make_model_args_mp(self):
+        """Prepare all combinations of parameter values for `run_all`
+        Due to multiprocessing requirements of @StaticMethod takes different input, hence the similar function
+        Returns:
+            List of list with the form:
+            [[model_object, dictionary_of_kwargs, max_steps, iterations]]
+        """
+        total_iterations = self.iterations
+        all_kwargs = []
+
+        count = len(self.parameters_list)
+        if count:
+            for i, params in enumerate(self.parameters_list):
+                kwargs = params.copy()
+                kwargs.update(self.fixed_parameters)
+                # run each iterations specific number of times
+                for iter in range(self.iterations):
+                    kwargs_repeated = kwargs.copy()
+                    all_kwargs.append(
+                        [self.model_cls, kwargs_repeated, self.max_steps, iter * count + i]
+                    )
+
+        elif len(self.fixed_parameters):
+            count = 1
+            kwargs = self.fixed_parameters.copy()
+            all_kwargs.append(kwargs)
+
+        total_iterations *= count
+
+        return all_kwargs, total_iterations
